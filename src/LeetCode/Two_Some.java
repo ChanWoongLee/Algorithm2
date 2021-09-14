7e9f561734bf7a59bdc4e27ad442bfc04f672b49
DESKTOP-49P942V\chanwoong <dlcksdnd6785@naver.com>
2021-09-14 23:37:26 +0900

    알고리즘 풀이

--- src/LeetCode/Two_Some.java
+++ src/LeetCode/Two_Some.java
@@ -2,7 +2,8 @@ package LeetCode;
 
 import java.util.Arrays;
 import java.util.HashMap;
-
+// 단순히 찾다 라는 개념을 순차적으로 탐색한다는 생각을 하지말고 컴퓨터를 이용해
+// hashmap으로 찾는다던가 여러 방법이 있다는것을 기억하자
 public class Two_Some {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> hm = new HashMap<>();
