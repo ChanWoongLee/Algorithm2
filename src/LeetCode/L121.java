a246c755baa306f3e917555a89dceb8c4ebdea67
DESKTOP-49P942V\chanwoong <dlcksdnd6785@naver.com>
2021-09-28 23:36:14 +0900

    알고리즘 풀이

---
+++ src/LeetCode/L121.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+public class L121 {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i = 0 ; i < prices.length; i++){
+            if(minPrice < prices[i]){
+                minPrice = prices[i];
+            } else if (profit > prices[i] - minPrice) {
+                profit = prices[i] - minPrice;
+            }
+        }
+        return profit;
+    }
+}
---
+++ src/test/Cloud_1.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Cloud_1 {
+    public static void main(String[] args) {
+        String a = "A";
+        char b = a.charAt(0);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+    }
+    static class  a{
+        int r;
+
+        public a(int r) {
+            this.r = r;
+        }
+    }
+}
