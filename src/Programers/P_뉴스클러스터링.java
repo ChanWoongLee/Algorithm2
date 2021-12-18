27756509bb00e010688590b67044128ae2d82513
DESKTOP-49P942V\chanwoong <dlcksdnd6785@naver.com>
2021-12-18 22:36:30 +0900

    알고리즘 문제풀이

--- src/Programers/P_뉴스클러스터링.java
+++ src/Programers/P_뉴스클러스터링.java
@@ -5,36 +5,39 @@ import java.util.HashMap;
 
 public class P_뉴스클러스터링 {
     public static void main(String[] args) {
-        System.out.println(solution("aa1+aa2","AAAA12"));
+        System.out.println(solution("FRANCE","french"));
+    }
+    static public boolean checkAlpah(char c){
+        if(c >= 65 && c <= 95){
+            return true;
+        }
+        return false;
     }
     static public int solution(String str1, String str2) {
         int answer = 0;
         str1 = str1.toUpperCase();
         str2 = str2.toUpperCase();
-        StringBuilder stb1 = new StringBuilder();
-        for(char c : str1.toCharArray()){
-            if(c >= 65 && c <= 95){
-                stb1.append(c);
-            }
-        }
-        StringBuilder stb2 = new StringBuilder();
-        for(char c : str2.toCharArray()){
-            if(c >= 65 && c <= 95){
-                stb2.append(c);
-            }
-        }
         ArrayList<String> str1Parse = new ArrayList<>();
-        for(int i = 0 ; i < str1.toString().toCharArray().length - 1; i++){
-            str1Parse.add(str1.toString().toCharArray()[i]+""+str1.toString().toCharArray()[i+1]+"");
+        char[] str1ToChar = str1.toCharArray();
+        for(int i = 0 ; i < str1ToChar.length - 1; i++){
+            char first = str1ToChar[i];
+            char second = str1ToChar[i+1];
+            if(checkAlpah(first) && checkAlpah(second)){
+                str1Parse.add(first+""+second+"");
+            }
         }
         HashMap<String, Integer> hm1 = new HashMap<>();
         for (String str : str1Parse) {
             hm1.put(str, hm1.getOrDefault(str, 0) +1);
         }
-
         ArrayList<String> str2Parse = new ArrayList<>();
-        for(int i = 0 ; i < stb2.toString().toCharArray().length - 1; i++){
-            str2Parse.add(stb2.toString().toCharArray()[i]+""+stb2.toString().toCharArray()[i+1]+"");
+        char[] str2ToChar = str2.toCharArray();
+        for(int i = 0 ; i < str2ToChar.length - 1; i++){
+            char first = str2ToChar[i];
+            char second = str2ToChar[i+1];
+            if(checkAlpah(first) && checkAlpah(second)){
+                str2Parse.add(first+""+second+"");
+            }
         }
         HashMap<String, Integer> hm2 = new HashMap<>();
         for (String str : str2Parse) {
