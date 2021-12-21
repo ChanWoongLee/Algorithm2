d14e7117ebabd7a0f17e82518698d6f14cbedcc3
DESKTOP-49P942V\chanwoong <dlcksdnd6785@naver.com>
2021-12-22 00:13:08 +0900

    알고리즘 문제풀이

--- src/Baekjun/B10800.java
+++ src/Baekjun/B10800.java
@@ -3,6 +3,8 @@ package Baekjun;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class B10800 {
@@ -10,6 +12,30 @@ public class B10800 {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         int N = Integer.parseInt(st.nextToken());
+        ArrayList<Ball> ar = new ArrayList<>();
+        for(int i = 0 ; i < N ; i ++) {
+            st = new StringTokenizer(bf.readLine());
+            ar.add(new Ball(i,Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())));
+        }
+        int[] players = new int[N];
+        int[] partSum = new int[N+1];
+        int sum = 0;
+        Collections.sort(ar,(a,b) -> a.size - b.size);
+        for(int i = 0; i < N ; i ++){
+            Ball now = ar.get(i);
+            int j = i+1;
+            while(j < N && now.size == ar.get(j).size){
+
+            }
+
+
+            sum += ar.get(i).size;
+            partSum[ar.get(i).color] += ar.get(i).size;
+            players[ar.get(i).idx] = sum - partSum[ar.get(i).color];
+        }
+        for(int p : players){
+            System.out.println(p);
+        }
     }
     static class Ball{
         int idx;
