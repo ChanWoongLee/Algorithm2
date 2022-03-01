f3f3ae0e6d63073b979a337e58a8ca151232fc00
DESKTOP-49P942V\chanwoong <dlcksdnd6785@naver.com>
2022-03-02 01:07:45 +0900

    알고리즘 문제풀이

--- src/Baekjun/B11779.java
+++ src/Baekjun/B11779.java
@@ -1,12 +1,11 @@
 package Baekjun;
 
+import com.sun.org.apache.xerces.internal.xs.ItemPSVI;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class B11779 {
     public static void main(String[] args) throws IOException {
@@ -16,6 +15,12 @@ public class B11779 {
         st = new StringTokenizer(bf.readLine());
         int m = Integer.parseInt(st.nextToken());
         int[][] map = new int[n + 1][n + 1];
+        int[] route = new int[n + 1];
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                map[i][j] = 987654321;
+            }
+        }
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(bf.readLine());
             int start = Integer.parseInt(st.nextToken());
@@ -29,21 +34,35 @@ public class B11779 {
 
         boolean[] visit = new boolean[n + 1];
         int[] dist = new int[n + 1];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[start] = 0;
+        Arrays.fill(dist, Integer.MAX_VALUE);
         visit[start] = true;
+        route[start] = 0;
+        dist[start] = 0;
         PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.value - b.value);
         pq.add(new Node(start, 0));
-
         while (!pq.isEmpty()) {
             Node now = pq.poll();
 
-            for (int i = 0; i < n + 1; i++) {
-                if(visit[i])
-                    continue;
-
+            for (int i = 1; i < n + 1; i++) {
+                if (dist[i] > now.value + map[now.node][i]) {
+                    dist[i] = now.value + map[now.node][i];
+                    pq.add(new Node(i, dist[i]));
+                    route[i] = now.node;
+                }
             }
         }
+        Stack<Integer> stack = new Stack<>();
+        int temp = end;
+        while(temp!=0){
+            stack.add(temp);
+            temp = route[temp];
+        }
+        System.out.println(dist[end]);
+        System.out.println(stack.size());
+        while(!stack.isEmpty()){
+            System.out.print(stack.pop()+" ");
+        }
+
     }
 
     static class Node {
